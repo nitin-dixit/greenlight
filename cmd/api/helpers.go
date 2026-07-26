7b163e2188ee5a1953edf3e3a6e696d8a540f552
Nitin Dixit <nitinajaydixit@gmail.com>
2026-07-26 13:51:44 +0530

    feat(api): add custom json field, json formatting and enveloped responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+type envelope map[string]any
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	idStr := r.PathValue("id")
 
@@ -19,7 +21,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		app.logger.Print(err)
