9b911a4eb29dd8b0ecaa99b3864f5a8e384c081a
Nitin Dixit <nitinajaydixit@gmail.com>
2026-08-06 16:27:47 +0530

    feat(movie): implement delete movie method and api handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,5 +9,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
 	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 	mux.HandleFunc("PUT /v1/movies/{id}", app.updateMovieHandler)
+	mux.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
 	return mux
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -81,7 +81,25 @@ func (m MovieModel) Update(ctx context.Context, movie *Movie) error {
 	return m.DB.QueryRow(ctx, query, args...).Scan(&movie.Version)
 }
 
-func (m MovieModel) Delete(id int64) error {
+func (m MovieModel) Delete(ctx context.Context, id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `delete from movies
+	where id = $1`
+
+	result, err := m.DB.Exec(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected := result.RowsAffected()
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
 
