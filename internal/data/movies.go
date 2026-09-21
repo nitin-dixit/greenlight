6f9379447a3bb5eff9a9284e04fbf629cd90aedf
Nitin Dixit <nitinajaydixit@gmail.com>
2026-09-21 23:01:14 +0530

    feat(api): implement pagination of the movies lists with limit and offset

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -133,12 +133,14 @@ func (m MovieModel) GetAll(ctx context.Context, title string, genres []string, f
         FROM movies
         WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
         AND (genres @> $2 OR $2 = '{}')     
-        ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+        ORDER BY %s %s, id ASC
+				LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
 
 	ctxWithTimeout, cancel := context.WithTimeout(ctx, 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.Query(ctxWithTimeout, query, title, genres)
+	args := []any{title, genres, filters.limit(), filters.offset()}
+	rows, err := m.DB.Query(ctxWithTimeout, query, args...)
 	if err != nil {
 		return nil, err
 	}
